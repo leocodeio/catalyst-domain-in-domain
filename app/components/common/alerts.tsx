c39c131ea4349fc1aadf9c0949b3ff87c3bcdf43
leocodeio <basicleo@787>
2024-09-15 08:03:43 +0530

    added timer to notifications

--- app/components/common/alerts.tsx
+++ app/components/common/alerts.tsx
@@ -1,8 +1,32 @@
 import { AlertTitle } from "@mui/material";
 import Alert from "@mui/material/Alert";
 import Stack from "@mui/material/Stack";
+import { useState, useEffect } from "react";
 
-export default function Alerts({ msg, code }: { msg: string; code: string }) {
+export default function Alerts({
+  msg,
+  code,
+  random,
+}: {
+  msg: string;
+  code: string;
+  random: number;
+}) {
+  const [visible, setVisible] = useState(true); // Use useState for visibility
+
+  useEffect(() => {
+    // Set the alert to be visible every time msg or code changes
+    setVisible(true);
+
+    // Set a timeout to hide the alert after 3 seconds
+    const timer = setTimeout(() => {
+      setVisible(false);
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, [msg, code, random]);
+
+  if (!visible) return null;
+
   return (
     <Stack
       sx={{
@@ -24,16 +48,6 @@ export default function Alerts({ msg, code }: { msg: string; code: string }) {
         <Alert variant="outlined" severity="error">
           <b>{msg}</b>
         </Alert>
-      )}
-      {false && (
-        <>
-          <Alert variant="outlined" severity="info">
-            {msg}
-          </Alert>
-          <Alert variant="outlined" severity="warning">
-            {msg}
-          </Alert>
-        </>
       )}
     </Stack>
   );
